1cebd8341fa749234b1771f59c6ab1e8697c529a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-08-15 19:46:57 +0700

    Refactor(Server): Config more security Endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,21 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app: Express = express();
 
+// App Config 
+dotenv.config();
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello API" });
