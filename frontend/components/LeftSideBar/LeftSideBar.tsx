c00d9fc97546a07cb2c79637be0f378d70eae18b
nguyebao281003 <nguyebao281003@gmail.com>
2024-08-18 09:31:00 +0000

    Feat(LeftSideBar): Add LeftSideBar Links Label

--- frontend/components/LeftSideBar/LeftSideBar.tsx
+++ frontend/components/LeftSideBar/LeftSideBar.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { leftSideBarLinks } from '@/constants';
+import { Button } from '../ui/button';
 
 const LeftSideBar = () => {
   return (
-    <div className='left_sidebar'>LeftSideBar</div>
+    <div className='left_sidebar'>
+      <div className='mt-2'>
+        {leftSideBarLinks.map((link) => (
+          <div className='px-4 py-3 border-[1px] border-slate-400 mx-4 my-2 rounded-lg' key={link.id}>
+            <h3>
+              {link.label}
+            </h3>
+          </div>
+        ))}
+      </div>
+
+      <Button className='mx-4 my-2'>
+        SignOut
+      </Button>
+    </div>
   );
 };
 
--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,5 +1,40 @@
 export const leftSideBarLinks = [
-
+    {
+        id: 1,
+        label: "Home",
+    }, 
+    {
+        id: 2,
+        label: "Search",
+    },
+    {
+        id: 3,
+        label: "Create Event",
+    },
+    {
+        id: 4,
+        label: "Explore",
+    },
+    {
+        id: 5,
+        label: "News",
+    },
+    {
+        id: 6,
+        label: "Activities",
+    },
+    {
+        id: 7,
+        label: "Chats",
+    },
+    {
+        id: 8,
+        label: "Setting",
+    },
+    {
+        id: 9,
+        label: "Profile",
+    },
 ];
 
 export const rightSideBarLinks = [
