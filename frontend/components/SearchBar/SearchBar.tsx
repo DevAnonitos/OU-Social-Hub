7202fb65efddf60888b216a6509cde221c260605
nguyebao281003 <nguyebao281003@gmail.com>
2024-08-18 08:45:48 +0000

    Feat(Search): Create Style SearchBar input Components

--- frontend/components/SearchBar/SearchBar.tsx
+++ frontend/components/SearchBar/SearchBar.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Input } from '../ui/input';
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <div className='flex items-center'>
+      <Input
+        placeholder='Search your favorite Event...' 
+        className='border-[1px] border-slate-300 rounded-md w-full h-10'
+      />
+    </div>
   );
 };
 
