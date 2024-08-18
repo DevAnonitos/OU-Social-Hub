7a5ae14315281d9827875196d3f9ffa40e4ed7de
nguyebao281003 <nguyebao281003@gmail.com>
2024-08-18 12:38:04 +0000

    Feat(RightSideBar): Add Style RightSideBar

--- frontend/components/RightSideBar/RightSideBar.tsx
+++ frontend/components/RightSideBar/RightSideBar.tsx
@@ -1,8 +1,65 @@
 import React from 'react';
+import Image from 'next/image';
 
 const RightSideBar = () => {
   return (
-    <div className='right_sidebar'>RightSideBar</div>
+    <div className='right_sidebar px-4'>
+      <section className='mt-4'>
+        <h3>
+          Up Coming Event!
+        </h3>
+        <div className='w-full h-[185px] border border-black mt-3'>
+          
+        </div>
+      </section>
+      <section className='flex flex-col pt-12'>
+        <h3>
+          Top Content Creator
+        </h3>
+        <div className='flex flex-col mt-1'>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-9 rounded-sm my-2'>
+            
+          </div>
+          
+        </div>
+      </section>
+    </div>
   );
 };
 
