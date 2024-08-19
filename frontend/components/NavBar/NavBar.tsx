d50af6ae6dd3918dc112bd701f626a2be48efed4
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-08-19 23:48:12 +0700

    Chore(NavBar): Add Icon Items NavBar component

--- frontend/components/NavBar/NavBar.tsx
+++ frontend/components/NavBar/NavBar.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import { Button } from '../ui/button';
+import { BellIcon } from "@radix-ui/react-icons"
 
 const NavBar = () => {
   return (
     <div className='navbar'>
       NavBar
       <SearchBar />
-      <Button>
-        Sign In
-      </Button>
+      <div className='flex items-center'>
+        <BellIcon width={25} height={25} className='mx-4 cursor-pointer' />
+        {/* Auth Button */}
+        <Button>
+          Sign In
+        </Button>
+      </div>
     </div>
   );
 };
