28d265bb3a2693bbac67cdf7d3da0fb2ba4480d8
nguyebao281003 <nguyebao281003@gmail.com>
2024-08-21 08:05:29 +0000

    Feat(Loader): Add LoaderSpinner Component

---
+++ frontend/components/Share/LoaderSpinner.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import { Loader } from "lucide-react";
+
+const LoaderSpinner = () => {
+    return (
+        <>
+            <div className="flex h-screen w-full">
+                <Loader className="animate-spin text-black" size={30} />
+            </div>
+
+        </>
+    );
+};
+
+export default LoaderSpinner;
