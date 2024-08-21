83348536a548434eb03ad6cf7bdbdccac8ff0375
nguyebao281003 <nguyebao281003@gmail.com>
2024-08-21 06:09:44 +0000

    Refactor(LeftSideBar): Add LeftSideBar Items Optimize Clean Code

--- frontend/components/LeftSideBar/LeftSideBar.tsx
+++ frontend/components/LeftSideBar/LeftSideBar.tsx
@@ -1,56 +1,15 @@
 "use client";
 
 import React from 'react';
-import { leftSideBarLinks } from '@/constants';
 import { Button } from '../ui/button';
-import Link from 'next/link';
-import Image from 'next/image';
-import { useRouter, usePathname } from 'next/navigation';
-
+import LeftSideBarItems from './LeftSideBarItems';
 
 const LeftSideBar = () => {
-
-  const router = useRouter();
-  const pathName = usePathname();
-
+  
   return (
     <div className='left_sidebar'>
       <div className='mt-2'>
-        {leftSideBarLinks.map((link) => {
-          const isActive =
-            (pathName.includes(link.route) && link.route.length > 1)
-            || pathName === link.route;
-  
-          return (
-            <Link 
-              key={link.id}
-              href={link.route} 
-              className={`left_sidebar-link ${isActive && "bg-slate-200"}`} 
-            >
-              <Image
-                src={link.icon}
-                alt={link.label}
-                width={26}
-                height={26}
-                className={``}
-              />
-              <h3 className='mx-4'>
-                {link.label}
-              </h3>
-            </Link>
-          )
-        })}
-        {/* LeftSideBar Responsive */}
-        {leftSideBarLinks.map((mobileLink) => (
-          <Link href={mobileLink.route} key={mobileLink.id} className='left_sidebar-mobilelink'>
-            <Image
-              src={mobileLink.icon}
-              alt={mobileLink.label}
-              width={26}
-              height={26}
-            />
-          </Link>
-        ))}
+        <LeftSideBarItems />
       </div>
 
       <Button className='mx-4 my-2 hidden lg:flex'>
