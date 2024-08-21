ad7655da0d19a3c9e073942c749bb26b98484917
nguyebao281003 <nguyebao281003@gmail.com>
2024-08-21 03:45:26 +0000

    Chore(Navbar): Add style css for SearchBar Input

--- frontend/components/SearchBar/SearchBar.tsx
+++ frontend/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,14 @@
+"use-client";
+
 import React from 'react';
 import { Input } from '../ui/input';
 
 const SearchBar = () => {
   return (
-    <div className='flex items-center'>
+    <div className='hidden flex-center rounded-full min-h-[54px] px-4 py-2 lg:flex'>
       <Input
-        placeholder='Search your favorite Event...' 
-        className='border-[1px] border-slate-300 rounded-md w-full h-10'
+        placeholder='Search your favorite Event ...' 
+        className='searchbar-input placeholder:text-grey-500 focus:border-[1px] focus-visible:ring-0 focus-visible:ring-offset-0'
       />
     </div>
   );
