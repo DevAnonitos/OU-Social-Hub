c51c3c482446aadedbc67dd4492e3d2a7a43fb8a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-08-23 23:52:04 +0700

    Feat(Loading): Add Loading UI to optimize loading page

--- frontend/components/Share/LoaderSpinner.tsx
+++ frontend/components/Share/LoaderSpinner.tsx
@@ -6,7 +6,7 @@ import { Loader } from "lucide-react";
 const LoaderSpinner = () => {
     return (
         <>
-            <div className="flex h-screen w-full">
+            <div className="flex h-screen items-center justify-center min-h-screen w-full">
                 <Loader className="animate-spin text-black" size={30} />
             </div>
 
