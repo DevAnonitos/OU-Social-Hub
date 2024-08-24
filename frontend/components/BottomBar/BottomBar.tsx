da96218e7f81aecbc80d3b41ec934eab4ddbd12c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-08-24 23:27:29 +0700

    Chore(BottomBar): Add Mapping Items BottomBar Icons

--- frontend/components/BottomBar/BottomBar.tsx
+++ frontend/components/BottomBar/BottomBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import BottomItems from './BottomItems';
 
 const BottomBar = () => {
   return (
     <div className='bottombar'>
-      BottomBar
+      <BottomItems />
     </div>
   );
 };
--- frontend/components/BottomBar/BottomItems.tsx
+++ frontend/components/BottomBar/BottomItems.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react';
 import { bottomBarLinks } from '@/constants';
 import Link from 'next/link';
@@ -5,9 +7,32 @@ import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
 const BottomItems = () => {
+
+  const pathName = usePathname();
+
   return (
-    <div>
-      {}
+    <div className='flex items-center justify-between gap-3 xs:gap-5 w-full'>
+      {bottomBarLinks.map((link) => {
+        const isActive =
+          (pathName.includes(link.route) && link.route.length > 1)
+          || pathName === link.route;
+  
+        return (
+          <Link 
+            key={link.id}
+            href={link.route} 
+            className={`bottombar_link ${isActive && "bg-slate-200"}`} 
+          >
+            <Image
+              src={link.icon}
+              alt={link.label}
+              width={28}
+              height={28}
+              className={``}
+            />
+          </Link>
+        )
+      })}
     </div>
   );
 };
