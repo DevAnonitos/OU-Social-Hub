114b7ea1e7a1af3cec4074ad661696d7b6b0c51c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-08-25 00:37:34 +0700

    Chore(RightSideBar): Add Top Content Creator to RighSideBar

--- frontend/components/RightSideBar/RightSideBar.tsx
+++ frontend/components/RightSideBar/RightSideBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import EventCarousel from '../Carousel/EventCarousel';
+import { Button } from '../ui/button';
 
 const RightSideBar = () => {
   return (
@@ -16,12 +17,98 @@ const RightSideBar = () => {
           Content Creator
         </h3>
         <div className='flex flex-col mt-1'>
-          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-14 rounded-sm my-2'>
-            
+          <div className='flex cursor-pointer items-center justify-between w-full h-14 rounded-sm my-2'>
+            <figure className='flex items-center gap-2'>
+              <Image
+                src={"/assets/images/dtdb.jpg"}
+                alt='User'
+                width={44}
+                height={44}
+                className='object-contain rounded-full'
+              />
+              <h3 className='text-14 font-semibold text-black'>
+                BaoAka
+              </h3>
+            </figure>
+            <Button>
+              Views
+            </Button>
           </div>
-          <div className='flex cursor-pointer justify-between border-[1px] w-full border-slate-400 h-14 rounded-sm my-2'>
-            
+
+          <div className='flex cursor-pointer items-center justify-between w-full h-14 rounded-sm my-2'>
+            <figure className='flex items-center gap-2'>
+              <Image
+                src={"/assets/images/dtdb.jpg"}
+                alt='User'
+                width={44}
+                height={44}
+                className='object-contain rounded-full'
+              />
+              <h3 className='text-14 font-semibold text-black'>
+                BaoAka
+              </h3>
+            </figure>
+            <Button>
+              Views
+            </Button>
           </div>
+
+          <div className='flex cursor-pointer items-center justify-between w-full h-14 rounded-sm my-2'>
+            <figure className='flex items-center gap-2'>
+              <Image
+                src={"/assets/images/dtdb.jpg"}
+                alt='User'
+                width={44}
+                height={44}
+                className='object-contain rounded-full'
+              />
+              <h3 className='text-14 font-semibold text-black'>
+                BaoAka
+              </h3>
+            </figure>
+            <Button>
+              Views
+            </Button>
+          </div>
+
+          <div className='flex cursor-pointer items-center justify-between w-full h-14 rounded-sm my-2'>
+            <figure className='flex items-center gap-2'>
+              <Image
+                src={"/assets/images/dtdb.jpg"}
+                alt='User'
+                width={44}
+                height={44}
+                className='object-contain rounded-full'
+              />
+              <h3 className='text-14 font-semibold text-black'>
+                BaoAka
+              </h3>
+            </figure>
+            <Button>
+              Views
+            </Button>
+          </div>
+
+          <div className='flex cursor-pointer items-center justify-between w-full h-14 rounded-sm my-2'>
+            <figure className='flex items-center gap-2'>
+              <Image
+                src={"/assets/images/dtdb.jpg"}
+                alt='User'
+                width={44}
+                height={44}
+                className='object-contain rounded-full'
+              />
+              <h3 className='text-14 font-semibold text-black'>
+                BaoAka
+              </h3>
+            </figure>
+            <Button>
+              Views
+            </Button>
+          </div>
+
+          
+
         </div>
       </section>
     </div>
