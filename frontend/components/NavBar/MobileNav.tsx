8d71944f1605e719f6682bed7f6d0f19195c2007
nguyebao281003 <nguyebao281003@gmail.com>
2024-08-25 09:10:37 +0000

    Chore(MobileNav): Add MobileNavItems to sidebar

--- frontend/components/NavBar/MobileNav.tsx
+++ frontend/components/NavBar/MobileNav.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { 
     Sheet,
     SheetContent,
@@ -6,23 +7,39 @@ import {
     SheetDescription,
     SheetFooter,
     SheetTrigger, 
+    SheetClose,
 } from '../ui/sheet';
 import Image from "next/image";
+import MobileNavItems from './MobileNavItems';
 
 import React from 'react';
 
 const MobileNav = () => {
+  
   return (
-    <nav className='md:hidden'>
+    <section className='md:hidden flex'>
         <Sheet>
             <SheetTrigger>
-                Menu
+              <Image src="/assets/icons/menu.svg" width={30} height={30} alt="menu" className="cursor-pointer" />
             </SheetTrigger>
-            <SheetContent>
-                
+            <SheetContent side="left" className="border-none bg-slate-50">
+                <SheetHeader>
+                    <SheetTitle>
+                        Menu
+                    </SheetTitle>
+                    <SheetDescription>
+                        Select an option
+                    </SheetDescription>
+                    <SheetClose asChild />
+                </SheetHeader>
+                <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
+                  <SheetClose asChild>
+                    <MobileNavItems />
+                  </SheetClose>
+                </div>
             </SheetContent>
         </Sheet>
-    </nav>
+    </section>
   );
 };
 
--- frontend/components/NavBar/NavBar.tsx
+++ frontend/components/NavBar/NavBar.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import { Button } from '../ui/button';
 import { BellIcon } from "@radix-ui/react-icons"
+import MobileNav from './MobileNav';
 
 const NavBar = () => {
   return (
     <div className='navbar'>
-      NavBar
+      <h1 className='hidden md:flex'>
+        NavBar
+      </h1>
+      <div>
+        <MobileNav />
+      </div>
       <SearchBar />
       <div className='flex items-center'>
         <BellIcon width={25} height={25} className='mx-4 cursor-pointer' />
