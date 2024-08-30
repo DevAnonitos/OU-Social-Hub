ea8caf600dbfd8f17a212f4b547daeea37f882f2
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-08-31 00:24:47 +0700

    Chore(Frontend): Add optimize Import Project

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+
 import LeftSideBar from "@/components/LeftSideBar/LeftSideBar";
 import RightSideBar from "@/components/RightSideBar/RightSideBar";
 import NavBar from "@/components/NavBar/NavBar";
