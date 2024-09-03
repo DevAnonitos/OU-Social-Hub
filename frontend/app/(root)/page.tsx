99d9d86d75b39d077d46389bf12d266dd6d1316c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-03 22:31:15 +0700

    Chore(LoadingUI): Add Loading UI to Page Routes

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,18 +1,17 @@
 
 import React from 'react';
-import EventCard from '@/components/Cards/EventCard';
 import BannerCarousel from '@/components/Carousel/BannerCarousel';
 import CollectionEvents from '@/components/Shared/CollectionEvents';
 
 const Home = () => {
   return (
     <main className='flex flex-col max-w-screen mx-auto w-full h-full min-h-screen'>
-        <div className='p-6 space-y-6 w-full h-full'>
-          <div className='flex items-center border w-full h-[300px]'>
-            <BannerCarousel/>
-          </div>
-          <CollectionEvents />
+      <div className='p-6 space-y-6 w-full h-full'>
+        <div className='flex items-center border w-full h-[300px]'>
+          <BannerCarousel/>
         </div>
+        <CollectionEvents />
+      </div>
     </main>
   );
 };
