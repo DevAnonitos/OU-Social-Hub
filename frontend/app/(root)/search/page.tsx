9911143e0c148fe073c33e3bd929bdb1d3cdf8c4
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-03 15:14:50 +0700

    Feat(SearchPage): Add CollectionEvent & SearchPage Style Components

--- frontend/app/(root)/search/page.tsx
+++ frontend/app/(root)/search/page.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import CollectionEvents from '@/components/Shared/CollectionEvents';
+import SearchBar from '@/components/SearchBar/SearchBar';
 
 const Search = () => {
   return (
-    <div>SearchPage</div>
+    <div className='flex flex-col max-w-screen mx-auto w-full h-full min-h-screen'>
+      <div className='p-6 space-y-6 w-full h-full flex flex-col wrapper'>
+        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+        <div className='flex w-full flex-col gap-5 md:flex-row'>
+          <div>
+            Search
+          </div>
+          <div className='flex items-center'>
+            Category
+          </div>
+        </div>
+        <CollectionEvents />
+      </div>
+    </div>
   );
 };
 
