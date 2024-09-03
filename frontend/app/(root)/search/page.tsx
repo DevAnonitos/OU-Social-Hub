51c1744f961fc8c48e38e92b4d0a434a344a8888
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-03 23:09:58 +0700

    Chore(EventPage): Add Title to EvenPage

--- frontend/app/(root)/search/page.tsx
+++ frontend/app/(root)/search/page.tsx
@@ -7,7 +7,7 @@ const Search = () => {
   return (
     <div className='flex flex-col max-w-screen mx-auto w-full h-full min-h-screen'>
       <div className='p-6 space-y-6 w-full h-full flex flex-col wrapper'>
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+        <h2 className="text-2xl font-bold">Trust by Thousands of Events 🎉</h2>
         <div className='flex w-full flex-col gap-5 md:flex-row'>
           <SearchEvent />
           <CategoryFilter />
