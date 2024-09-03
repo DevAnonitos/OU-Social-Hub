6f9d110c16cf7de49417f16eb97cada16a836b28
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-03 21:09:57 +0700

    Chore(CategoryFilter): Add SelectItems to CategoryFilter

--- frontend/components/Shared/CategoryFilter.tsx
+++ frontend/components/Shared/CategoryFilter.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import {
   Select, 
-  SelectContent, 
+  SelectContent,   
   SelectItem, 
   SelectTrigger, 
   SelectValue 
@@ -12,7 +12,20 @@ import {
 const CategoryFilter = () => {
   return (
     <Select>
-      
+      <SelectTrigger className='w-full bg-grey-50 h-[54px] placeholder:text-grey-500 rounded-full p-regular-16 px-5 py-3 border-none focus-visible:ring-transparent focus:ring-transparent'>
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="All" className="py-3 cursor-pointer  focus:bg-grey-50 p-regular-14">
+          All
+        </SelectItem>
+        <SelectItem value="Dev" className="py-3 cursor-pointer  focus:bg-grey-50 p-regular-14">
+          Dev
+        </SelectItem>
+        <SelectItem value="Media" className="py-3 cursor-pointer  focus:bg-grey-50 p-regular-14">
+          Media
+        </SelectItem>
+      </SelectContent>
     </Select>
   );
 };
