687da2dc4ab6aaae2b1be1d7e457cc29344baaa4
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-06 00:21:08 +0700

    Chore(Comment): Add Form, Input, Button to Comment

--- frontend/components/Comment/CommentInput.tsx
+++ frontend/components/Comment/CommentInput.tsx
@@ -1,4 +1,15 @@
+"use client";
+
 import React from 'react';
+import * as z from 'zod';
+import { 
+  Form, 
+  FormControl, 
+  FormItem, 
+  FormField
+} from '../ui/form';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
 
 const CommentInput = () => {
   return (
