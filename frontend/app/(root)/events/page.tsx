e37e826e341edea74b4701d63fb0312a17655411
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-05 11:29:33 +0700

    Feat(Comment): Add Comment Component to EventDetail

--- frontend/app/(root)/events/page.tsx
+++ frontend/app/(root)/events/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from "next/image";
+
 import EventDetailCard from '@/components/Cards/EventDetailCard';
 
 const EventsDetail = () => {
---
+++ frontend/components/Comment/EventComment.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EventComment = () => {
+  return (
+    <div>EventComment</div>
+  );
+};
+
+export default EventComment;
