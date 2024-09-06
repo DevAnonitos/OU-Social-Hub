0de3ace52699a462b21a096e7d15fedb554f4ec1
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-06 16:17:21 +0700

    Chore(CommentInput): Add  FormField Comment Input to EventComment

--- frontend/components/Comment/CommentInput.tsx
+++ frontend/components/Comment/CommentInput.tsx
@@ -6,14 +6,62 @@ import {
   Form, 
   FormControl, 
   FormItem, 
-  FormField
+  FormField,
+  FormDescription, 
+  FormLabel, 
+  FormMessage,
 } from '../ui/form';
-import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
 import { Button } from '../ui/button';
+import Image from 'next/image';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+const formSchema = z.object({
+  content: z.string().min(3, {
+    message: "Comment must have at least 3 characters",
+  }),
+});
 
 const CommentInput = () => {
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
   return (
-    <div>CommentInput</div>
+    <div>
+      <Form {...form}>
+        <form className='space-y-8 after:clear-both after:table'>
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div className='flex items-center space-x-2'>
+                    <Image 
+                      src={"/assets/images/dtdb.jpg"}
+                      alt="hero image"
+                      width={44}
+                      height={44}
+                      className="object-cover rounded-full"
+                    />
+                    <Textarea
+                      placeholder='Write a comment ...'
+                      className='rounded-2xl border-[1px] border-slate-400 focus-visible:ring-transparent focus-visible:ring-offset-0'
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type='submit' className='float-right'>
+            Comment
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 };
 
--- frontend/components/Comment/EventComment.tsx
+++ frontend/components/Comment/EventComment.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import EventCommentItems from './EventCommentItems';
+import CommentInput from './CommentInput';
+import Image from 'next/image';
 
 const EventComment = () => {
   return (
-    <div>
+    <div className='flex flex-col gap-5 w-full bg-grey-50 px-5 py-4 rounded-xl'>
+      <div className='flex flex-end items-center justify-between'>
+        <h2 className='text-2xl font-bold flex items-center'>
+          <Image
+            src={"/assets/icons/comment.svg"}
+            width={28}
+            height={28}
+            alt='Comment'
+            className='mr-2'
+          />
+          Comment
+        </h2>
+        <h3 className='text-lg font-semibold'>
+          11555: Comments
+        </h3>
+      </div>
+      <CommentInput />
       <EventCommentItems />
     </div>
   );
