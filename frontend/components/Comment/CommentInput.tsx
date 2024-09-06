712ae14619a9f2fcbb28599112dd6848a1dd61b2
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-06 21:22:58 +0700

    Fix(CommentInput): Add FormMessage to FormField

--- frontend/components/Comment/CommentInput.tsx
+++ frontend/components/Comment/CommentInput.tsx
@@ -53,6 +53,7 @@ const CommentInput = () => {
                     />
                   </div>
                 </FormControl>
+                <FormMessage />
               </FormItem>
             )}
           />
