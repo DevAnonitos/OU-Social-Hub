88aa65c451f698c90f0b183e1007fecb5cff0009
nguyebao281003 <nguyebao281003@gmail.com>
2024-09-12 04:12:28 +0000

    Feat(AuthRoute): Add AuthRoute to Routing API

---
+++ backend/src/routes/v1/auth.route.ts
@@ -0,0 +1,6 @@
+import express, { Router } from "express";
+import { signIn, signUp } from "../../controllers/auth.controller";
+
+const router = express.Router();
+
+export default router;
--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -1,5 +1,10 @@
 import express, { Router } from "express";
+import authRoute from "./auth.route";
 
 const routes: Router = express.Router();
+
+// using Routes
+
+routes.use("/auth", authRoute);
 
 export default routes;
