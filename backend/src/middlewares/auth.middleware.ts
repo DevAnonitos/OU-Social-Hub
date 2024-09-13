c034ed4b06a35c3321ca57d5ca558ab59918fdf7
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-14 00:23:15 +0700

    Feat(AuthMiddleware): Add Auth.Middleware File COnfig

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,9 +1,35 @@
+import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
-export const auth = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        
-    } catch (error: any) {
-        
+export const authMiddleware = async (
+    req: Request, res: Response, next: NextFunction
+) => {
+    const token = req.headers['authorization']?.split('')[1];
+    if(!token) {
+        return res.status(401).json({
+            message: "No token is provided",
+        });
     }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+        (req as any).user = decoded;
+        next();
+    } catch (error: any) {
+        return res.status(401).json({
+            message: "Invalid token",
+        });
+    }
+};
+
+export const adminMiddleware = async (
+    req: Request, res: Response, next: NextFunction
+) => {
+    const user = (req as any).user;
+    if(user.role !== "ADMIN") {
+        return res.status(403).json({
+            message: "Access Denied"
+        })
+    }
+    next();
 };
