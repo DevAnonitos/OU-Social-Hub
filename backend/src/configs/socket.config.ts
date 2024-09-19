5cfc1d73f5fd594cc095340be581c8644684301e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-20 01:04:41 +0700

    Feat(GetUsers): Add GetUsers Realtime

--- backend/src/configs/socket.config.ts
+++ backend/src/configs/socket.config.ts
@@ -1,18 +1,31 @@
 import { Server } from "socket.io";
 import http from "http";
+import prisma from "./prisma.config";
 
 let io: Server | null = null;
 
 export const createSocketServer = (server: http.Server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"]
+      origin: "https://3000-idx-ou-social-hub-1721398872129.cluster-qpa6grkipzc64wfjrbr3hsdma2.cloudworkstations.dev",
+      methods: ["GET", "POST"],
     },
   });
 
-  io.on("connection", (socket) => {
+  io.on("connection", async (socket) => {
     console.log(`User connected: ${socket.id}`);
+
+    // Function to send users to the client
+    const sendUsers = async () => {
+      try {
+        const users = await prisma.user.findMany();
+        socket.emit("users", users);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    sendUsers();
 
     socket.on("disconnect", () => {
       console.log(`User disconnected: ${socket.id}`);
--- backend/src/routes/v1/user.route.ts
+++ backend/src/routes/v1/user.route.ts
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
 import { getUsers } from "../../controllers/user.controller";
+import { authMiddleware } from "../../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.get("/getusers", getUsers);
-
+router.use(authMiddleware)
 
 export default router;
