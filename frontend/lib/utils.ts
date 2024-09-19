ea97a63fc874e62616ad53e30b513fd87c359143
nguyebao281003 <nguyebao281003@gmail.com>
2024-09-19 14:03:26 +0000

    Feat(utils): Add formatTime Func

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -23,3 +23,10 @@ export const getTokenExpiryDate = (token: string): number | null => {
 };
 
 
+export const formatTime = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+  return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+};
+
+
--- frontend/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-
-export const config = {
-    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
-   
