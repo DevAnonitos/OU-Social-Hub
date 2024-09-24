dbf6e27568d6c5b4f59a99c82c924285ac857232
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-25 00:04:22 +0700

    Feat(EventRoute): Add Set Event API Route

--- backend/src/controllers/event.controller.ts
+++ backend/src/controllers/event.controller.ts
@@ -1,9 +1,38 @@
 import prisma from "../configs/prisma.config";
 import { Request, Response } from "express";
+import { CreateUserParams } from "../interfaces";
 
 export const createEvent = async (req: Request, res: Response) => {
   try {
-    
+    const { userId, event }: CreateUserParams = req.body;
+
+    const existingEvent = await prisma.events.findFirst({
+      where: {
+        eventTitle: event.title,
+        startDateTime: event.startDateTime,
+      },
+    });
+
+    if (existingEvent) {
+      return res.status(409).json({
+        error: "An event already exists."
+      });
+    }
+
+    const createEvent = await prisma.events.create({
+      data: {
+        eventTitle: event.title,
+        eventDescription: event.description,
+        eventLocation: event.location,
+        imageUrl: event.imageUrl,
+        linkUrl: event.url,
+        startDateTime: event.startDateTime,
+        endDateTime: event.endDateTime,
+        organizerId: userId,
+      },
+    });
+    console.log(createEvent);
+    res.status(200).json(createEvent);
   } catch (error: any) {
     console.error(error);
   }
--- backend/src/routes/v1/event.route.ts
+++ backend/src/routes/v1/event.route.ts
@@ -1,8 +1,10 @@
 import express, { Router } from "express";
+import { createEvent } from "../../controllers/event.controller";
 import { authMiddleware } from "../../middlewares/auth.middleware";
 
 const router = express.Router();
 
+router.post("/create", createEvent);
 router.use(authMiddleware);
 
 export default router;
