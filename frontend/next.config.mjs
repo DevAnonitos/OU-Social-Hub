31c44c903ebca38b661039d145e24540f0e97cf8
nguyebao281003 <nguyebao281003@gmail.com>
2024-09-25 15:48:24 +0000

    Feat(Uploadthing): Add setup package Uploadthing

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,6 +10,16 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['utfs.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: ''
+      }
+    ]
   }
 };
 
