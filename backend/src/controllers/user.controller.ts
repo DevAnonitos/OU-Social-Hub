f7b3212e6cc7231b5174a88b1ca35f59cb22a5c4
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-09-28 00:09:41 +0700

    Feat(UserRoute): Add getUserByMonth Route

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -29,3 +29,47 @@ export const getUsers = async (req: Request, res: Response) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const getUsersByMonth = async (req: Request, res: Response) => {
+  try {
+    const currentYear = new Date().getFullYear();
+
+    // Truy vấn dựa trên tháng và năm
+    const userCount = await prisma.user.groupBy({
+      by: ["createAt"], // Sử dụng đúng tên trường trong schema của bạn
+      _count: {
+        id: true,
+      },
+      where: {
+        createAt: {
+          gte: new Date(`${currentYear}-01-01T00:00:00.000Z`),
+          lt: new Date(`${currentYear + 1}-01-01T00:00:00.000Z`),
+        },
+      },
+    });
+
+    const months = Array.from({ length: 12 }, (_, index) => {
+      const monthDate = new Date(currentYear, index);
+      const monthName = monthDate.toLocaleString("default", { month: "long" });
+
+      // Tìm số lượng user theo tháng
+      const monthData = userCount.filter((uc) => {
+        const createdMonth = new Date(uc.createAt).getMonth();
+        return createdMonth === index;
+      });
+
+      // Tính tổng số user cho tháng này
+      const totalCount = monthData.reduce((total, uc) => total + uc._count.id, 0);
+
+      return {
+        month: monthName,
+        count: totalCount,
+      };
+    });
+
+    res.status(200).json({ data: months });
+  } catch (error: any) {
+    console.error("Error fetching user counts by month", error);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+};
--- backend/src/routes/v1/user.route.ts
+++ backend/src/routes/v1/user.route.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
-import { getUsers } from "../../controllers/user.controller";
+import { getUsers,getUsersByMonth } from "../../controllers/user.controller";
 import { authMiddleware } from "../../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.get("/getusers", getUsers);
+router.get("/count-by-month", getUsersByMonth);
 router.use(authMiddleware);
 
 export default router;
